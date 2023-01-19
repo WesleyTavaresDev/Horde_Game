b6bd2ae016d1ffa149789ef5f2b3f8cbd5bdba51
Wesley Tavares <wesleyTavaresDev@outlook.com>
2023-01-19 20:52:27 -0300

    Jump and Fall Animation

--- Beatriz_Game/Assets/2.Scripts/Player/PlayerJumpController.cs
+++ Beatriz_Game/Assets/2.Scripts/Player/PlayerJumpController.cs
@@ -8,9 +8,11 @@ public class PlayerJumpController : MonoBehaviour
     [SerializeField] private Vector2 checkerRadius;
     [SerializeField] private float jumpForce;
     [SerializeField] private float fallForce;
+    [SerializeField] private AnimationClip landAnimation;
 
-    [SerializeField] private Rigidbody2D rb;
     [SerializeField] private BoxCollider2D coll;
+    private Rigidbody2D rb;
+    private Animator anim;
     private PlayerInput input;
     
     void Awake()
@@ -18,6 +20,7 @@ public class PlayerJumpController : MonoBehaviour
         coll = GetComponent<BoxCollider2D>();
         input = GetComponent<PlayerInput>();
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void OnEnable() => JumpInput().started += OnJump;
@@ -34,15 +37,32 @@ public class PlayerJumpController : MonoBehaviour
             Fall();
     }
 
-
-    private void Fall()
+    private void LateUpdate()
     {
-        rb.velocity += Vector2.down * fallForce * Time.deltaTime;
+        if(IsGrounded())
+            anim.SetBool("Fall", false);
     }
+
+
 
     private void Jump()
     {
         rb.AddForce(Vector2.up * jumpForce * Time.fixedDeltaTime, ForceMode2D.Impulse);
+        anim.SetBool("Jump", true);
+    }
+
+    private void Fall()
+    {
+        anim.SetBool("Jump", false);
+        anim.SetBool("Fall", true);
+        rb.velocity += Vector2.down * fallForce * Time.deltaTime;     
+    }
+
+    private IEnumerator Land()
+    {
+        anim.SetBool("Land", true);
+        yield return new WaitForSeconds(landAnimation.length);
+        anim.SetBool("Land", false);
     }
 
     public bool IsGrounded() => Physics2D.BoxCast(new Vector2(coll.bounds.center.x , transform.position.y), checkerRadius, 0, Vector2.zero, 0, 1 << 7);
