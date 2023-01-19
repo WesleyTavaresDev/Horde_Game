7a2d6a0ed2cb6abf8826b3d30e4f773892a69467
Wesley Tavares <wesleyTavaresDev@outlook.com>
2023-01-19 20:19:00 -0300

    PlayerJumpController -> jump and fall added

--- Beatriz_Game/Assets/2.Scripts/Player/PlayerController.cs
+++ Beatriz_Game/Assets/2.Scripts/Player/PlayerController.cs
@@ -7,12 +7,10 @@ namespace Player
 { 
     public class PlayerController : MonoBehaviour
     {
-        
+
         [HideInInspector] public StateMachine playerSM;
         [HideInInspector] public IdleState idleState;
-        [HideInInspector] public RunState runState;
-        [HideInInspector] public JumpState jumpState;
-
+        [HideInInspector] public RunState runState; 
         [HideInInspector] public PlayerInput input;
         [HideInInspector] public InputAction move;
         [SerializeField] private Vector2 checkerRadius;
@@ -39,7 +37,6 @@ namespace Player
             playerSM = new();
             idleState = new(this, playerSM);
             runState = new(this, playerSM);
-            jumpState = new(this, playerSM);
 
             playerSM.Initialize(idleState);
         }
@@ -69,22 +66,6 @@ namespace Player
             rb.AddForce(force * Time.deltaTime, forceMode2D);
         }
 
-        public InputAction GetJumpInput() => input.actions["Jump"];        
         #endregion
-        
-        #region checker
-
-        public bool IsGrounded()
-        {
-            return Physics2D.BoxCast(new Vector2(coll.bounds.center.x , transform.position.y), checkerRadius, 0, Vector2.zero, 0, 1 << 7);
-        }
-
-        #endregion
-        
-
-        void OnDrawGizmos()
-        {
-            Gizmos.DrawWireCube(new Vector2(coll.bounds.center.x , transform.position.y), checkerRadius);
-        }
     }
 }
--- Beatriz_Game/Assets/2.Scripts/Player/PlayerJumpController.cs
+++ Beatriz_Game/Assets/2.Scripts/Player/PlayerJumpController.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerJumpController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Vector2 checkerRadius;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float fallForce;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private BoxCollider2D coll;
+    private PlayerInput input;
+    
+    void Awake()
     {
-        
+        coll = GetComponent<BoxCollider2D>();
+        input = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable() => JumpInput().started += OnJump;
+   
+    private void OnJump(InputAction.CallbackContext context)
     {
-        
+        if(IsGrounded())
+            Jump();
     }
+
+    private void FixedUpdate()
+    {
+        if(rb.velocity.y < 0f)
+            Fall();
+    }
+
+
+    private void Fall()
+    {
+        rb.velocity += Vector2.down * fallForce * Time.deltaTime;
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce * Time.fixedDeltaTime, ForceMode2D.Impulse);
+    }
+
+    public bool IsGrounded() => Physics2D.BoxCast(new Vector2(coll.bounds.center.x , transform.position.y), checkerRadius, 0, Vector2.zero, 0, 1 << 7);
+    
+    private InputAction JumpInput() => input.actions["Jump"];        
+    
+    private void OnDrawGizmos() => Gizmos.DrawWireCube(new Vector2(coll.bounds.center.x , transform.position.y), checkerRadius);
 }
