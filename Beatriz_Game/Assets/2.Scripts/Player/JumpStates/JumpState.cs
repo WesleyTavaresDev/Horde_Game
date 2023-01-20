068378e50fe768075b88016c82fcb6f92662d3fd
Wesley Tavares <wesleyTavaresDev@outlook.com>
2023-01-20 16:54:31 -0300

    Jump mechanic
    Moved from PlayerJumpController -> to -> JumpState

--- Beatriz_Game/Assets/2.Scripts/Player/JumpStates/JumpState.cs
+++ Beatriz_Game/Assets/2.Scripts/Player/JumpStates/JumpState.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class JumpState : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float jumpForce;
+    [SerializeField] private Vector2 checkerRadius;
+    [SerializeField] private BoxCollider2D coll;
+
+    private PlayerInput input;
+    private Rigidbody2D rb;
+    private PlayerJumpController jumpController;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        input = GetComponent<PlayerInput>();
+        coll = GetComponent<BoxCollider2D>();
+        jumpController = GetComponent<PlayerJumpController>();
+
+        JumpInput().started += OnJump;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnJump(InputAction.CallbackContext context)
     {
-        
+        if(IsGrounded()) 
+            Jump();
     }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce * Time.fixedDeltaTime, ForceMode2D.Impulse);
+        jumpController.PlayJumpAnimation();
+    }
+
+    private InputAction JumpInput() => input.actions["Jump"];
+
+    public bool IsGrounded() => Physics2D.BoxCast(new Vector2(coll.bounds.center.x , transform.position.y), checkerRadius, 0, Vector2.zero, 0, 1 << 7);
+
+    private void OnDisable() => JumpInput().started -= OnJump;
+    private void OnDrawGizmos() => Gizmos.DrawWireCube(new Vector2(coll.bounds.center.x , transform.position.y), checkerRadius);
 }
--- Beatriz_Game/Assets/2.Scripts/Player/PlayerJumpController.cs
+++ Beatriz_Game/Assets/2.Scripts/Player/PlayerJumpController.cs
@@ -8,11 +8,8 @@ public class PlayerJumpController : MonoBehaviour
     enum ANIMATION_JUMP_STATE{Inactive, Jumping, Falling, Landing};
     [SerializeField] private ANIMATION_JUMP_STATE animationJumpState;
 
-    [SerializeField] private Vector2 checkerRadius;
-    [SerializeField] private float jumpForce;
     [SerializeField] private float fallForce;
     [SerializeField] private AnimationClip landAnimation;
-    [SerializeField] private BoxCollider2D coll;
 
     private const float LAND_CHECKER_DISTANCE = -2f;
     private Rigidbody2D rb;
@@ -21,19 +18,12 @@ public class PlayerJumpController : MonoBehaviour
     
     void Awake()
     {
-        coll = GetComponent<BoxCollider2D>();
         input = GetComponent<PlayerInput>();
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
-    private void OnEnable() => JumpInput().started += OnJump;
-   
-    private void OnJump(InputAction.CallbackContext context)
-    {
-        if(IsGrounded())
-            Jump();
-    }
+    public void PlayJumpAnimation() => animationJumpState = ANIMATION_JUMP_STATE.Jumping; 
 
     private void Update()
     {
@@ -79,12 +69,6 @@ public class PlayerJumpController : MonoBehaviour
         }
     }
 
-    private void Jump()
-    {
-        rb.AddForce(Vector2.up * jumpForce * Time.fixedDeltaTime, ForceMode2D.Impulse);
-        animationJumpState = ANIMATION_JUMP_STATE.Jumping;
-    }
-
     private void Fall()
     {
         rb.velocity += Vector2.down * fallForce * Time.fixedDeltaTime;     
@@ -97,11 +81,6 @@ public class PlayerJumpController : MonoBehaviour
         yield return new WaitForSeconds(landAnimation.length);
         anim.SetBool("Land", false);
         animationJumpState = ANIMATION_JUMP_STATE.Inactive;
-    }
-
-    public bool IsGrounded() => Physics2D.BoxCast(new Vector2(coll.bounds.center.x , transform.position.y), checkerRadius, 0, Vector2.zero, 0, 1 << 7);
+    }      
     
-    private InputAction JumpInput() => input.actions["Jump"];        
-    
-    private void OnDrawGizmos() => Gizmos.DrawWireCube(new Vector2(coll.bounds.center.x , transform.position.y), checkerRadius);
 }
