38a767cc4657de8e2e61168c7b40b229dfb0f8b4
Wesley Tavares <wesleyTavaresDev@outlook.com>
2023-01-21 20:19:31 -0300

    MeleeAttributes

---
+++ Beatriz_Game/Assets/2.Scripts/Player/MeleeAttributes.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttributes : MonoBehaviour
+{
+    [SerializeField] private int damage;
+    public int Damage {get; private set;}
+}
