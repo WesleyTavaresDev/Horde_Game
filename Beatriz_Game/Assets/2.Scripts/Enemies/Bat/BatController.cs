29771632343e6072f359f5e52430e51aca0d52f2
Wesley Tavares <wesleyTavaresDev@outlook.com>
2023-01-22 14:55:29 -0300

    BatController script created

---
+++ Beatriz_Game/Assets/2.Scripts/Enemies/Bat/BatController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy.Bat
+{
+    public class BatController : MonoBehaviour
+    {
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+    }
+}
