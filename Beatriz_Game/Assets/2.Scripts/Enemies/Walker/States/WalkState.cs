3a02d8823019580d9d5b9c947c6e1e65437a99ec
Wesley Tavares <wesleyTavaresDev@outlook.com>
2023-01-28 01:25:53 -0300

    WalkState script
    WalkerState deleted;
    
    WalkState created

--- Beatriz_Game/Assets/2.Scripts/Enemies/Walker/States/WalkState.cs
+++ Beatriz_Game/Assets/2.Scripts/Enemies/Walker/States/WalkState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WalkerState : MonoBehaviour
+public class WalkState : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
