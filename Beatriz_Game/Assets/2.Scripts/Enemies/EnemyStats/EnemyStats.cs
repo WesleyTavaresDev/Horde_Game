43b86d8dfb1add7c8c47d05586633f81c0f85715
Wesley Tavares <wesleyTavaresDev@outlook.com>
2023-01-27 22:05:54 -0300

    EnemyController class created

--- Beatriz_Game/Assets/2.Scripts/Enemies/EnemyStats/EnemyStats.cs
+++ Beatriz_Game/Assets/2.Scripts/Enemies/EnemyStats/EnemyStats.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyStats : MonoBehaviour
+[CreateAssetMenu(fileName = "EnemyStat"
+)]
+public class EnemyStats : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public List<EnemyStatsInfo> stats = new();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public float GetStat(EnemyStatsEnum statType)
+   {
+        foreach(var stat in  stats)
+        {
+            if(stat.statsType == statType)
+                return stat.value;
+        }
+
+        Debug.LogError($"There's no {statType}");
+        return 0;
+   }
 }
--- Beatriz_Game/Assets/2.Scripts/Enemies/EnemyStats/EnemyStatsInfo.cs
+++ Beatriz_Game/Assets/2.Scripts/Enemies/EnemyStats/EnemyStatsInfo.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyStatsInfo : MonoBehaviour
+[System.Serializable]
+public struct EnemyStatsInfo
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public EnemyStatsEnum statsType;
+   public float value;
 }
