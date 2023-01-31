450a7257a9a0bfb4c1bb90acc373c01a2fc68c82
Wesley Tavares <wesleyTavaresDev@outlook.com>
2023-01-31 16:36:54 -0300

    Hit

--- Beatriz_Game/Assets/2.Scripts/Enemies/Walker/WalkerStateMachine.cs
+++ Beatriz_Game/Assets/2.Scripts/Enemies/Walker/WalkerStateMachine.cs
@@ -15,13 +15,17 @@ public class WalkerStateMachine : MonoBehaviour
 
     public float reactTime;
     public float idleTime;
+    public float hitTime;
     public AnimationClip attackClip;
 
+    public float life;
     [HideInInspector] public WalkState walkState;
     [HideInInspector] public IdleState idleState;
     [HideInInspector] public ReactState reactState;
     [HideInInspector] public AttackState attackState;
     [HideInInspector] public HittedState hittedState;
+    
+    private EnemyController enemyController;
     public Animator anim;
     public Rigidbody2D rb;
     private StateMachine walkerSM;
@@ -30,6 +34,9 @@ public class WalkerStateMachine : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
 
+        enemyController = GetComponent<EnemyController>();
+        
+        life = enemyController.enemyStats.GetStat(EnemyStatsEnum.healthPoints);
         startPosition = this.gameObject.transform.position.x;
         
         points.Add(startPosition + distanceToWalk);
@@ -43,6 +50,7 @@ public class WalkerStateMachine : MonoBehaviour
         walkState = new(this, walkerSM);
         reactState = new(this, walkerSM);
         attackState = new(this, walkerSM);
+        hittedState = new(this, walkerSM);
 
         walkerSM.Initialize(walkState);
     }
@@ -60,6 +68,24 @@ public class WalkerStateMachine : MonoBehaviour
     public bool IsPlayerClose() => Physics2D.OverlapBox((new Vector2(rb.worldCenterOfMass.x + (0.5f * GetDirection()).x, rb.worldCenterOfMass.y)), sensorSize, 0, 1 << 6);
 
     public Vector2 GetDirection() => transform.localScale.x >= 0 ? Vector2.right : Vector2.left;
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.gameObject.CompareTag("PlayerAttack"))
+        {
+            SubstractLife(other.GetComponentInParent<Player.PlayerAttackController>().damage);
+
+            if(!IsDead())
+                walkerSM.ChangeState(hittedState);
+        }
+    }
+
+    private void SubstractLife(float damage)
+    {
+        life -= damage;
+    }
+
+    private bool IsDead() => life <= 0; 
 
     private void OnDrawGizmos() 
     {
