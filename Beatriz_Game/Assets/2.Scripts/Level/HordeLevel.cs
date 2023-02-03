6af63093cbf4ee67bf42a27831c1054b7e27312a
Wesley Tavares <wesleyTavaresDev@outlook.com>
2023-02-03 16:09:38 -0300

    spawnEffect

--- Beatriz_Game/Assets/2.Scripts/Level/HordeLevel.cs
+++ Beatriz_Game/Assets/2.Scripts/Level/HordeLevel.cs
@@ -9,6 +9,7 @@ public class HordeLevel : MonoBehaviour
     [SerializeField] private List<Horde> hordes = new();
     
     [SerializeField] private uint quantityOfEnemies;
+    [SerializeField] private Effect spawnEffect;
 
     public void Init()
     {
@@ -22,7 +23,11 @@ public class HordeLevel : MonoBehaviour
         { 
             for(int j = 0; j < hordes[currentHorde].enemies[i].quantity; j++)
             {
-                Instantiate(hordes[currentHorde].enemies[i].enemy, hordes[currentHorde].enemies[i].spawnerArea.GetRandomPosition(), Quaternion.identity);
+                Vector2 randomPos = hordes[currentHorde].enemies[i].spawnerArea.GetRandomPosition();
+                var effect = Instantiate(spawnEffect.gameObject, new Vector2(randomPos.x, randomPos.y + 0.5f), Quaternion.identity) as GameObject ;
+                effect.GetComponent<Effect>().Run();
+                
+                Instantiate(hordes[currentHorde].enemies[i].enemy, randomPos, Quaternion.identity);
                 quantityOfEnemies++;
             }
         }
